6a418140a8c40ea400c5d14b8cf6893efa1d1401
Jakub Psarski <psarski@student.agh.edu.pl>
2025-03-22 00:09:38 +0100

    lab1 done

---
+++ lab1/ex4b.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def logistic_map_single(x, r):
+    return np.float32(r * x * (1 - x))
+
+
+def logistic_map_double(x, r):
+    return r * x * (1 - x)
+
+
+r_values = [3.75, 3.76, 3.77, 3.78, 3.79, 3.8]
+x0 = 0.4
+iterations = 100
+
+
+for r in r_values:
+    plt.figure(figsize=(12, 6))
+
+    x_single = np.float32(x0)
+    x_double = float(x0)
+
+    x_vals_single = []
+    x_vals_double = []
+
+    for _ in range(iterations):
+        x_single = logistic_map_single(x_single, np.float32(r))
+        x_double = logistic_map_double(x_double, r) 
+        x_vals_single.append(x_single)
+        x_vals_double.append(x_double)
+
+    plt.scatter(range(iterations), x_vals_single, label=f'r={r:.2f} (single)', marker='.', color="orange")
+    plt.plot(range(iterations), x_vals_double, label=f'r={r:.2f} (double)', marker='.')
+
+    for i in range(iterations):
+        color = 'g' if x_vals_single[i] > x_vals_double[i] else 'r'
+        plt.plot([i, i], [x_vals_single[i], x_vals_double[i]], color, linewidth=0.5)
+
+    plt.xlabel("Iterations")
+    plt.ylabel("$x_n$")
+    plt.title("Single and double precision trajectory comparison")
+    plt.legend()
+
+plt.show()
+
+# Initially, both precisions produce similar results, but differences become evident after multiple iterations.
+# Single precision accumulates rounding errors faster than double precision, leading to trajectory divergence over iterations.
---
+++ lab1/ex4c.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def count_iterations_to_zero(x0, iter_limit, r=4, dtype=np.float32):
+    x = dtype(x0)
+    for i in range(iter_limit):
+        x = dtype(r * x * (1 - x))
+        if x == 0:
+            print(f"x0 = {x0} converged to 0 after {i} iterations.")
+            return i
+    return iter_limit
+
+x_values = np.linspace(0.001, 0.999, 100)
+iter_limit = int(input("Enter the iteration limit: "))
+iterations_needed = [count_iterations_to_zero(x, iter_limit) for x in x_values]
+
+plt.figure(figsize=(10, 5))
+plt.plot(x_values, iterations_needed, marker="o", linestyle="")
+plt.xlabel("$x_0$")
+plt.ylabel("Iterations")
+plt.title("Iterations to reach zero for r=4")
+plt.show()
+
+# Some x0 values (15/100) need a relatively small amount of iterations (<=2200) to converge to 0.
+# For the rest of the values the system remains in chaos for over 1000000 iterations.
